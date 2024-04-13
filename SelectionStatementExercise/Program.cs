be7fa470dbecbc1e1988e1395725612249365285
mark aldridge <mark.aldridge1@protonmail.com>
2024-04-13 12:41:40 -0700

    Guess favorite number game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Net.WebSockets;
+
+var r = new Random();
+var favNumber = r.Next(1, 1000);
+int guess;
+
+do
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine("Guess my favorite number, between 1 and 1000");
+    guess = int.Parse(Console.ReadLine());
 }
+while (!checkGuess(guess));
+
+bool checkGuess(int guess)
+{
+    bool isCorrect = false;
+    if (guess < favNumber)
+    {
+        Console.WriteLine("Too low!");
+    }
+    else if (guess > favNumber)
+    {
+        Console.WriteLine("Too high");
+    }
+    else if (guess == favNumber)
+    {
+        Console.WriteLine($"You got it! My favorite number is {favNumber}");
+        isCorrect = true;
+    }
+    return isCorrect;
+}
+
+
