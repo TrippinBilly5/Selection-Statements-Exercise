3242a47da4fbd6d8fc0b40d5d14f3c148e0090d2
mark aldridge <mark.aldridge1@protonmail.com>
2024-04-13 15:56:58 -0700

    Add subject conversation

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,31 +1,57 @@
-﻿var r = new Random();
-var favNumber = r.Next(1, 1000);
-int guess;
+﻿//var r = new Random();
+//var favNumber = r.Next(1, 1000);
+//int guess;
 
-do
+//do
+//{
+//    Console.WriteLine("Guess my favorite number, between 1 and 1000");
+//    guess = int.Parse(Console.ReadLine());
+//}
+//while (!checkGuess(guess));
+
+//bool checkGuess(int guess)
+//{
+//    bool isCorrect = false;
+//    if (guess < favNumber)
+//    {
+//        Console.WriteLine("Too low!");
+//    }
+//    else if (guess > favNumber)
+//    {
+//        Console.WriteLine("Too high");
+//    }
+//    else if (guess == favNumber)
+//    {
+//        Console.WriteLine($"You got it! My favorite number is {favNumber}");
+//        isCorrect = true;
+//    }
+//    return isCorrect;
+//}
+
+
+Console.WriteLine("What is your favorite school subject?");
+string subject = Console.ReadLine();
+
+switch (subject.ToLower())
 {
-    Console.WriteLine("Guess my favorite number, between 1 and 1000");
-    guess = int.Parse(Console.ReadLine());
+    case "pe":
+    case "physical education":
+        Console.WriteLine("There's an easy A");
+        break;
+    case "english":
+        Console.WriteLine("So you like to write");
+        break;
+    case "math":
+        Console.WriteLine("I like numbers, too");
+        break;
+    case "literature":
+        Console.WriteLine("A reader, I see");
+        break;
+    case "history":
+        Console.WriteLine("The past is interesting");
+        break;
+    default:
+        Console.WriteLine("I don't know that subject");
+        break;
 }
-while (!checkGuess(guess));
-
-bool checkGuess(int guess)
-{
-    bool isCorrect = false;
-    if (guess < favNumber)
-    {
-        Console.WriteLine("Too low!");
-    }
-    else if (guess > favNumber)
-    {
-        Console.WriteLine("Too high");
-    }
-    else if (guess == favNumber)
-    {
-        Console.WriteLine($"You got it! My favorite number is {favNumber}");
-        isCorrect = true;
-    }
-    return isCorrect;
-}
-
 
